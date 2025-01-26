14c26df0092927a9c02765a5a58e4e0054bd6688
Oleksandr Rybalov <xanderrybalov@gmail.com>
2025-01-26 19:31:27 +0200

    feat: added logic to header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,5 +1,16 @@
+<script lang="ts">
+const today: Date = new Date();
+
+const monthName: string = today.toLocaleString('en-US', {
+    month: 'long'
+});
+const dayName: string = today.toLocaleString('en-US', {
+    weekday: 'long'
+});
+const day: number = today.getDate();
+</script>
+
 <div class="text-center p-container-margin border-b-header-line border-header-line-primary">
-    <h1 class="text-4xl font-bold text-task-pending">December</h1>
-    <p class="text-sm text-secondary-violett font-header-semibold pt-task-gap">Tuesday, December 22</p>
-  </div>
-  
+    <h1 class="text-4xl font-bold text-task-pending">{monthName}</h1>
+    <p class="text-sm text-secondary-violett font-header-semibold pt-task-gap">{dayName}, {monthName} {day}</p>
+</div>
